b988cb8c18928f3c75ffd6f8f2663ba1e4579421
risclxm <risclxm@163.com>
2018-06-22 15:22:54 +0800

    增加age属性

--- GitProject/src/com/test/po/Dog.java
+++ GitProject/src/com/test/po/Dog.java
@@ -2,6 +2,15 @@ package com.test.po;
 
 public class Dog {
   private String name;
+  private int age;
+
+public int getAge() {
+	return age;
+}
+
+public void setAge(int age) {
+	this.age = age;
+}
 
 public String getName() {
 	return name;
