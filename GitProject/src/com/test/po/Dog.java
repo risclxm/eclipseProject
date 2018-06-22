22bb7674e60d9c8f979da6f82ca35bccd7efb314
risclxm <risclxm@163.com>
2018-06-22 15:37:52 +0800

    回退并提交

--- GitProject/src/com/test/po/Dog.java
+++ GitProject/src/com/test/po/Dog.java
@@ -2,15 +2,6 @@ package com.test.po;
 
 public class Dog {
   private String name;
-  private int age;
-
-public int getAge() {
-	return age;
-}
-
-public void setAge(int age) {
-	this.age = age;
-}
 
 public String getName() {
 	return name;
